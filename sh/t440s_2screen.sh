8722136f538a753af2f163ba7e29341d256d2682
anthony cantor <ar.cantor@gmail.com>
2015-02-25 02:35:45 -0800

    added t440s xrandr config scripts

---
+++ sh/t440s_2screen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off \
+       --output eDP1 --mode 1920x1080 --pos 0x1200 --rotate normal \
+       --output DP1 --off \
+       --output HDMI2 --off \
+       --output HDMI1 --off \
+       --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal \
+       --output DP3 --off \
+       --output DP4 --off \
+       --output DP5 --off
