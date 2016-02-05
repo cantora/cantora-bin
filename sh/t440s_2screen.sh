6cefb47e387dab1b6af97afd44846c222d69f2d6
anthony cantor <cantora@gubbi.mobi>
2016-02-05 00:41:01 -0800

    disable unused screen on t440s

--- sh/t440s_2screen.sh
+++ sh/t440s_2screen.sh
@@ -4,6 +4,7 @@ xrandr --output VIRTUAL1 --off \
        --output DP1 --off \
        --output HDMI2 --off \
        --output HDMI1 --off \
+       --output DP2-1 --off \
        --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal \
        --output DP3 --off \
        --output DP4 --off \
